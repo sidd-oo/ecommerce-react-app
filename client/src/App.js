1e7265d036647f59233d4dca4fa02ef7d911f3ec
sidd-oo <sahoosiddharth3@gmail.com>
2022-01-08 12:19:55 +0530

    feat: Created Announcement component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import Announcemnent from './components/Announcemnent';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Announcemnent/>
       <Navbar/>
     </div>
   );
