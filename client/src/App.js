0d72da4df64b244d120e004ae4db7e43d3de6fb9
sidd-oo <sahoosiddharth3@gmail.com>
2022-01-09 07:49:25 +0530

    feat: Created React Slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Announcemnent from './components/Announcemnent';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div>
       <Announcemnent/>
       <Navbar/>
+      <Slider/>
     </div>
   );
 }
---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import styled from "styled-components";
+import {
+  KeyboardArrowLeftOutlined,
+  KeyboardArrowRightOutlined,
+} from "@mui/icons-material";
+import { sliderItems } from "../dummyData";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  display: flex;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+  console.log(slideIndex);
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <KeyboardArrowLeftOutlined style={{ fontSize: "60px" }} />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => {
+          return (
+            <Slide bg={item.bg} key={item.id}>
+              <ImgContainer>
+                <Image src={item.img} />
+              </ImgContainer>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Description>{item.desc}</Description>
+                <Button>SHOP NOW</Button>
+              </InfoContainer>
+            </Slide>
+          );
+        })}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <KeyboardArrowRightOutlined style={{ fontSize: "60px" }} />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
