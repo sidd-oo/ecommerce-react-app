c4a7790f9bb748bfbae0a24d1c52af547e9a51a8
sidd-oo <sahoosiddharth3@gmail.com>
2022-01-10 06:17:44 +0530

    feat: Created Footer design component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Announcemnent from './components/Announcemnent';
 import Categories from './components/Categories';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Newsletter from './components/Newsletter';
 import Products from './components/Products';
@@ -14,6 +15,7 @@ function App() {
       <Categories/>
       <Products/>
       <Newsletter/>
+      <Footer/>
     </div>
   );
 }
