fc7dd62581ab814aaa8570eb10f324d44cac14ae
sidd-oo <sahoosiddharth3@gmail.com>
2022-01-10 11:10:58 +0530

    feat: Created Category page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,9 @@
-import Announcemnent from './components/Announcemnent';
-import Categories from './components/Categories';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Newsletter from './components/Newsletter';
-import Products from './components/Products';
-import Slider from './components/Slider';
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div>
-      <Announcemnent/>
-      <Navbar/>
-      <Slider/>
-      <Categories/>
-      <Products/>
-      <Newsletter/>
-      <Footer/>
+      <ProductList/>
     </div>
   );
 }
