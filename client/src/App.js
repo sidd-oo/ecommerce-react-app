f392debb32142d1c8b79273b9591efb473bcf003
sidd-oo <sahoosiddharth3@gmail.com>
2022-05-05 05:23:55 +0530

    feat: added routes to the ecommerce app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div>
-      <Product/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={ user ? <Home/> : <Login />} />
+        <Route path="/register" element={user ? <Home/> : <Register />} />
+      </Routes>
+    </Router>
   );
 }
 
