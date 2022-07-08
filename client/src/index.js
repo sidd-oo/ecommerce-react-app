84d2eb3dc136a7d4f22d44fa01afedd18e823eb0
sidd-oo <sahoosiddharth3@gmail.com>
2022-07-08 15:02:00 +0530

    feat: redux persist the user and cart details in the browser

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from 'react-redux'
-import store from './redux/store';
+import { Provider } from 'react-redux'
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
