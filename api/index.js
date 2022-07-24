746125cde8aefa681ad4f7c92fc4e6ce0110c03f
sidd-oo <sahoosiddharth3@gmail.com>
2022-07-24 17:33:26 +0530

    deploy: Passing the proxies directly

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose
     console.log(err);
   });
 
-app.use(cors({ origin: [process.env.ADMIN_PROXY, process.env.CLIENT_PROXY] }));
+app.use(cors({ origin: ["https://ecommerce-react-app-admin.vercel.app/", "https://ecommerce-react-app-pearl.vercel.app/"] }));
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
