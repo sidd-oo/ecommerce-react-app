bed8429a9474f018ee3320f4e3d4211764f098a8
Siddharth Sahoo <sahoosiddharth3@gmail.com>
2023-05-27 04:37:04 +0530

    fix: cors all proxy

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose
   });
 
 app.use(cors());  
-// app.use(cors({ origin: [process.env.ADMIN_PROXY, process.env.CLIENT_PROXY] }));
+app.use(cors({ origin: [process.env.ADMIN_PROXY, process.env.CLIENT_PROXY] }));
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
